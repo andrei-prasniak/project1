cd3608ecc7dee35b0624cf998ccfdbe84a3d940a
andreiprasniak <andreiprasniak@gmail.com>
2016-09-02 11:52:01 +0000

    two routes added,two methods added to controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  root#index
+  root 'item#index'
+  get 'item/new' => 'item#new', as: 'item_new'
+  post 'item/new' => 'item#create'
   get 'item/:id' => 'item#show', as: 'item_show'
 
 
