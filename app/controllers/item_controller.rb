3199ba4a7eb1c8e28c47030aa9e1cc2ea355cddd
andreiprasniak <andreiprasniak@gmail.com>
2016-09-02 22:31:49 +0000

    edit form was added to view

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -28,4 +28,13 @@ class ItemController < ApplicationController
     render 'new'
     end
   end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+    redirect_to root_path
+    else
+    render 'edit'
+    end
+  end
+
 end
