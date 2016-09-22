9eca0f5af94e4ab69fd7b199733ef810f84bf412
andreiprasniak <andreiprasniak@gmail.com>
2016-09-22 20:59:10 +0000

    colon moved

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
-  validates :name, presence :true
+  validates :name, presence: true
   belongs_to :type
   belongs_to :countryofmake
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,4 +1,4 @@
 class Type < ActiveRecord::Base
-  validates :description, presence :true
+  validates :description, presence: true
   has_many :items
 end
